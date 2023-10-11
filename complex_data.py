42890d60179ea99df456c1db420288654c926676
Ysble-yushu <gys200922@163.com>
2023-10-11 15:56:22 +0200

    use more complex data for calorie counter

---
+++ complex_data.py
@@ -0,0 +1,94 @@
+meals = [
+    {
+        "id": "meal-1",
+        "name": "hamburger",
+        "calories": 600,
+        "price": 5
+    },
+    {
+        "id": "meal-2",
+        "name": "cheese burger",
+        "calories": 750,
+        "price": 7
+	},
+    {
+        "id": "meal-3",
+        "name": "veggie burger",
+        "calories": 400,
+        "price": 6
+	},
+    {
+        "id": "meal-4",
+        "name": "vegan burger",
+        "calories": 350,
+        "price": 6
+	},
+    {
+        "id": "meal-5",
+        "name": "sweet potatoes",
+        "calories": 230,
+        "price": 3
+	},
+    {
+        "id": "meal-6",
+        "name": "salad",
+        "calories": 15,
+        "price": 4
+	},
+    {
+        "id": "meal-7",
+        "name": "iced tea",
+        "calories": 70,
+        "price": 2
+	},
+    {
+        "id": "meal-8",
+        "name": "lemonade",
+        "calories": 90,
+        "price": 2
+	}
+]
+
+combos = [
+    {
+        "id": "combo-1",
+        "name": "cheesy combo",
+        "meals": ["meal-2", "meal-5", "meal-8"],
+        "price": 11,
+    },
+    {
+        "id": "combo-2",
+        "name": "veggie combo",
+        "meals": ["meal-3", "meal-5", "meal-7"],
+        "price": 10,
+    },
+    {
+        "id": "combo-3",
+        "name": "vegan combo",
+        "meals": ["meal-4", "meal-6", "meal-8"],
+        "price": 10,
+    }
+]
+
+def calories_counter(*items):
+    total = 0
+    for item in items:
+        if item in meals.keys:
+            total += meals[item]
+        elif item in combos.keys:
+            total += combos[item]
+        else:
+            print(f"{item} not in menu")
+    return total
+
+def price_calorie_counter(*items):
+    total = 0
+    for item in items:
+        if item in meals.keys():
+            total += meals[item]
+        elif item in combos.keys:
+           total += combos[item]
+        else:
+            print(f"{item} is not in the menu")
+    return total
+
